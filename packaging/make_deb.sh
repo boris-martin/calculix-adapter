d449f67f8f82031d6731937711582a8af5b640f6
Boris Martin <boris.martin.be@gmail.com>
2021-10-19 23:57:22 +0200

    Fixed all lintian warnings. Added draft of manpage

--- packaging/make_deb.sh
+++ packaging/make_deb.sh
@@ -6,6 +6,14 @@ cp changelog.Debian calculix-precice_2.17-1_amd64/usr/share/doc/calculix-precice
 gzip -9 calculix-precice_2.17-1_amd64/usr/share/doc/calculix-precice/changelog.Debian -f -n
 strip --strip-unneeded calculix-precice_2.17-1_amd64/usr/bin/ccx_preCICE
 
+#Compile and compress the manual
+
+pandoc manpage.md -s -t man -o ccx_preCICE.1
+mkdir -p calculix-precice_2.17-1_amd64/usr/share/man/man1
+gzip -9 -n -f ccx_preCICE.1
+chmod 644 ccx_preCICE.1.gz
+mv ccx_preCICE.1.gz calculix-precice_2.17-1_amd64/usr/share/man/man1
+
 dpkg-deb --build --root-owner-group calculix-precice_2.17-1_amd64
 lintian *.deb
 
