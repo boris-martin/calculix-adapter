19e7ddc7901c14546ea9c8784e1dcc6bc2d92d3b
Boris Martin <boris.martin.be@gmail.com>
2021-10-19 22:41:36 +0200

    Polishing to improve linting. Man page still missing

--- packaging/make_deb.sh
+++ packaging/make_deb.sh
@@ -2,7 +2,8 @@
 
 # Compress the changelog, strip the binaries
 cp changelog.Debian calculix-precice_2.17-1_amd64/usr/share/doc/calculix-precice/changelog.Debian
-gzip -9 calculix-precice_2.17-1_amd64/usr/share/doc/calculix-precice/changelog.Debian -f
+# Options : -f for removing file if it was there, -n for no time stamp
+gzip -9 calculix-precice_2.17-1_amd64/usr/share/doc/calculix-precice/changelog.Debian -f -n
 strip --strip-unneeded calculix-precice_2.17-1_amd64/usr/bin/ccx_preCICE
 
 dpkg-deb --build --root-owner-group calculix-precice_2.17-1_amd64
